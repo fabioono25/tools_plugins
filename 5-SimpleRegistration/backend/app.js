1307f3921708351e9851b5f570312ede0e1c5918
fabioono25 <fabioono25@gmail.com>
2022-10-07 20:28:29 +0200

    fix connection + using latest versions

--- 5-SimpleRegistration/backend/app.js
+++ 5-SimpleRegistration/backend/app.js
@@ -1,14 +1,21 @@
 const express = require('express')
-const restful = require('node-restful')
-const server = express()
-const mongoose = restful.mongoose
+//const restful = require('node-restful')
+// const server = express()
+//const mongoose = restful.mongoose
+const mongoose = require('mongoose')
 
 // DB (mongoose using the promises API from Node)
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://db/mydb') //port: 27017
+// mongoose.Promise = global.Promise
+mongoose
+  .connect('mongodb://db/mydb') //port: 27017
+  .then(() => {
+      const server = express()
 
-// Test
-server.get('/', (req, res, next) => res.send('Backend'))
+      // Test
+      server.get('/', (req, res, next) => res.send('Backend'))
 
-// Server
-server.listen(3000)
+      // Server
+      server.listen(3000, () => {
+        console.warn('Server has started!')
+      })
+  })
