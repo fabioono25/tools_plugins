4df45805ffa5c78115251a9cf908e7f71dd6b6fa
fabioono25 <fabioono25@gmail.com>
2022-10-09 16:19:10 +0200

    adding a queue (Redis) + workers to send email

--- 6-EmailSender/app/sender.py
+++ 6-EmailSender/app/sender.py
@@ -4,44 +4,42 @@ import json
 import os
 from bottle import route, run, Bottle, request
 
-#class Sender(Bottle):
-    # def __init__(self):
-    #     super().__init__()
-    #     self.route('/', method='POST', callback=self.send)
+class Sender(Bottle):
+    def __init__(self):
+        super().__init__()
+        self.route('/', method='POST', callback=self.send)
         
     #     redis_host = os.getenv('REDIS_HOST', 'queue')
-    #     self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
+        self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
 
     #     db_host = os.getenv('DB_HOST', 'db')
     #     db_user = os.getenv('DB_USER', 'postgres')
     #     db_name = os.getenv('DB_NAME', 'sender')
     #     dsn = f'dbname={db_name} user={db_user} host={db_host}'
-    #     self.conn = psycopg2.connect(dsn)
+        DSN = 'dbname=email_sender user=postgres host=db password=postgres'        
+        self.conn = psycopg2.connect(DSN)
 
-DSN = 'dbname=email_sender user=postgres host=db password=postgres'        
-def register_message(title, message):
-    SQL = 'INSERT INTO emails (title, message) VALUES (%s, %s)'
-    conn = psycopg2.connect(DSN)
-    cur = conn.cursor()
-    cur.execute(SQL, (title, message))
-    conn.commit()
-    cur.close()
+    def register_message(self, title, message):
+        SQL = 'INSERT INTO emails (title, message) VALUES (%s, %s)'
+        cur = self.conn.cursor()
+        cur.execute(SQL, (title, message))
+        self.conn.commit()
+        cur.close()
 
-    # msg = {'title': title, 'message': message}
-    # self.fila.rpush('sender', json.dumps(msg))
+        msg = {'title': title, 'message': message}
+        self.fila.rpush('sender', json.dumps(msg))
 
-    print('message registrada !')
+        print('message registrada !')
 
-@route('/', method='POST')
-def send():
-    title = request.forms.get('title')
-    message = request.forms.get('message')
+    def send(self):
+        title = request.forms.get('title')
+        message = request.forms.get('message')
 
-    register_message(title, message)
-    return 'message is queued ! title: {} message: {}'.format(
-        title, message
-    )
+        self.register_message(title, message)
+        return 'message is queued ! title: {} message: {}'.format(
+            title, message
+        )
 
 if __name__ == '__main__':
-    #sender = Sender()
-    run(host='0.0.0.0', port=8080, debug=True)
+    sender = Sender()
+    sender.run(host='0.0.0.0', port=8080, debug=True)
---
+++ 6-EmailSender/worker/worker.py
@@ -0,0 +1,16 @@
+import redis
+import json
+import os
+from time import sleep
+from random import randint
+
+if __name__ == '__main__':
+    # redis_host = os.getenv('REDIS_HOST', 'queue')
+    r = redis.Redis(host='queue', port=6379, db=0)
+    print('Waiting for messages ...')
+    while True:
+        mensagem = json.loads(r.blpop('sender')[1])
+        # Simulando envio de e-mail...
+        print('Sending a mensagem:', mensagem['title'])
+        sleep(randint(15, 25))
+        print('Message', mensagem['title'], 'sent')
